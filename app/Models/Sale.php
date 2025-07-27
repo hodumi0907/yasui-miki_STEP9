4daa74eed211f682f1770187f0c57f01a8c3c685
yasuimiki <hodumi0907@gmail.com>
2025-07-28 00:30:41 +0900

    Sales.phpをSale.phpにリネーム

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -5,9 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sales extends Model
+class Sale extends Model
 {
     use HasFactory;
+
+    protected $fillable = [ //データベースに追加が許可されている属性
+        'user_id',
+        'product_id',
+        'quantity',
+    ];
 
     public function products()
     {
