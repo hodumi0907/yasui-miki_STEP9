68098d4ab67a218d30a76efe9d210edfd9d33f45
yasuimiki <hodumi0907@gmail.com>
2025-08-24 16:04:25 +0900

    storeをsendに変更し送信エラー時のメッセージを追加

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use App\Mail\ContactMail;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -24,8 +26,7 @@ class ContactController extends Controller
      */
     public function create()
     {
-        //お問い合わせフォームを表示
-        return view(`contact`);
+        return view('contact');
     }
 
     /**
@@ -34,10 +35,19 @@ class ContactController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
-    {
-        //
+    public function send(ContactRequest $request) //storeでも可
+{
+    $data = $request->validated();
+
+    try {
+        Mail::to('test@test.com')->send(new ContactMail($data));
+        // 成功時は index.blade.php にリダイレクト
+        return redirect()->route('products.index')->with('success', 'お問い合わせ内容を送信しました。');
+    } catch (\Exception $e) {
+        \Log::error('お問い合わせメール送信エラー: '.$e->getMessage());
+        return back()->with('error', 'メール送信に失敗しました。');
     }
+}
 
     /**
      * Display the specified resource.
@@ -47,8 +57,9 @@ class ContactController extends Controller
      */
     public function show($id)
     {
-        //送信時のフラッシュメッセージ
-        return redirect()->route(`products.index`)->with(`success`,`お問い合わせ内容を送信しました。`);
+        //問い合わせフォームを表示
+        return view('contact.show');
+
     }
 
     /**
